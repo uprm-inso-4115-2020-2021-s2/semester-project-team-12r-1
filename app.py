b02fbd1e598afa0df0631d1b0317ac9edfb9c280
John Robles <john.robles1@upr.edu>
2021-04-27 14:02:10 -0400

    cleaning code

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ CORS(app)
 def hello_world():
     return 'Hello World!'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
